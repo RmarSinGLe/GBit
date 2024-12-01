ef133faa3d1b04acc94b73f06a09b9e021238fd2
aidendenden233 <2996876880@qq.com>
2024-12-01 17:20:00 +0800

    下巴朝向

--- Assets/TechDiffusion/Script/GamePlay/Monster.cs
+++ Assets/TechDiffusion/Script/GamePlay/Monster.cs
@@ -64,6 +64,13 @@ public class Monster : MonoBehaviour
         }
 
         isDashing = false; 
+        
+        if (player != null)
+        {
+            Vector3 targetDir = player.transform.position - transform.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
     }
 
     private void DashTowardsPlayer()
